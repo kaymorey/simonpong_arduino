678010b539e4e62eb68a536c3979adca93e071d4
CAYLA Bertrand <bcayla@outlook.com>
2015-01-19 12:11:33 +0100

    Add button on Arduino

--- processing_arduino/processing_arduino.ino
+++ processing_arduino/processing_arduino.ino
@@ -2,11 +2,17 @@
    from: http://itp.nyu.edu/physcomp/Labs/AnalogIn
 —————————————————————*/
  
-int potPinTop = 0;    // Analog input pin that the potentiometer is attached to
-int potValueTop = 0;  // value read from the pot
+int potPinLeft = 0;    // Analog input pin that the potentiometer is attached to
+int potValueLeft = 0;  // value read from the pot
 
-int potPinBottom = 1;    // Analog input pin that the potentiometer is attached to
-int potValueBottom = 0;  // value read from the pot
+int potPinRight = 1;    // Analog input pin that the potentiometer is attached to
+int potValueRight = 0;  // value read from the pot
+
+const int buttonPinLeft_1 = 8;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+// variables will change:
+int buttonLeftState_1 = 0;         // variable for reading the pushbutton status
 
 String stringToSend;
  
@@ -17,9 +23,10 @@ void setup() {
 }
  
 void loop() {
-  potValueTop = analogRead(potPinTop); // read the pot value
-  potValueBottom = analogRead(potPinBottom); // read the pot value
-  stringToSend = String(potValueTop/4)+"$"+String(potValueBottom/4);
+  potValueLeft = analogRead(potPinLeft); // read the pot value
+  potValueRight = analogRead(potPinRight); // read the pot value
+  buttonLeftValue_1 = digitalRead(buttonPinLeft_1);
+  stringToSend = String(potValueLeft/4)+"$"+String(potValueRight/4);
   Serial.println(stringToSend);      // print the pot value back to the debugger pane
   delay(30);                     // wait 10 milliseconds before the next loop
 }
